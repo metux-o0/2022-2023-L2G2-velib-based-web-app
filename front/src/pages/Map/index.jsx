73bacebc151daa34929a24af7d74768907af9d41
Céline Ye <ye2707@outlook.com>
2023-03-14 16:26:30 +0100

    ajout de la carte de paris avec stations

--- front/src/pages/Map/index.jsx
+++ front/src/pages/Map/index.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
-export default function SimpleMap(){
+import { stations } from "../../datas/stations";
+import config from './config.json'
+const apikey=config.googlemapkey;
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+export default function Carte(){
   const defaultProps = {
     center: {
       lat: 48.8607882,
       lng: 2.3492268
     },
-    zoom: 13
+    zoom: 13,
   };
 
   return (
-    // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%'}}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAvdUOTN-1ChKx-bX7D7C6UsETuWHYI8Xg" }}
+        bootstrapURLKeys={{key:apikey}}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
+        <AnyReactComponent
+          lat={stations.lat}
+          lng={stations.lng}
+          text="📍"
+        />
       </GoogleMapReact>
     </div>
   );
-}
+};
