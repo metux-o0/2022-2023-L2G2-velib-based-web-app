9d3e6030078488dc9defd16b5377dc51e3b35fa9
unknown <shi.lisa0127@gmail.com>
2023-03-28 15:53:16 +0200

    Lisa
    Ajout sur les pages d'accueil et contact

--- front/src/pages/Contact/index.jsx
+++ front/src/pages/Contact/index.jsx
@@ -1,7 +1,11 @@
 function Contact() {
   return (
   <div>
-      <h1>Contact</h1>
+      <h1>Contacter Service Client</h1>
+      <h4>0176491234</h4>
+      <h4>Service disponible du lundi au vendredi de 8h à 22h, le samedi de 9h à 22h et le dimanche et jours fériés de 9h à 19h.
+          <br></br>Coût d'un appel local</h4>
+
     </div>
   )
 }
--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -2,10 +2,46 @@ import '../../styles/Home.css'
 function Home() {
     return ( 
     <div>
-        <h3>Le Déplacement le plus agréable</h3>
-        <h4>&bull;Nos vélos sont là pour vous emmener ou vous voulez</h4>
-        <h4>&bull;Déplacez vous en un rien de temps grâce à nos vélos - pour nous trajet en douceur, sans effort et en toute sécurité</h4>
-    </div>
+        <h2>5 choses à savoir sur les Vélib’</h2>
+        <h4>&bull;Le Vélib’ bleu est électrique, le Vélib’ vert est mécanique.</h4>
+        <h4>&bull;Pour prendre ou restituer un Vélib’, tout se passe depuis le boîtier inséré dans le guidon du vélo.</h4>
+        <h4>&bull;Pour changer le niveau d'assistance des Vélib’ électriques, appuyez sur les touches 1, 2 ou 3 du boîtier.</h4>
+        <h4>&bull;Soyez attentifs aux symboles affichés sur l’écran : ils vous indiquent les étapes et les actions à effectuer.</h4>
+        <h4>&bull;Téléchargez l’application Vélib’ pour afficher la carte des stations et des vélos disponibles.</h4>
+        <br></br>
+        <h3>J'active ma carte Vélib’ ou Navigo pour la première fois</h3>
+        <iframe width="447" height="315" src="https://www.youtube.com/embed/HR-WQd07tJk" title="J&#39;active ma carte Vélib’ ou Navigo pour la première fois" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je prends un Vélib’ avec ma carte</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/poc2ycUmtIY" title="Je prends un Vélib&#39; avec ma carte (Vélib&#39; ou Navigo)" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je prends un Vélib’ avec mes codes</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/ihNw84lseEw" title="Je prends un Vélib’ avec mes codes" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je prends un Vélib' avec mon smartphone (Android uniquement)</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/jmenGGmZPKA" title="Je prends un Vélib&#39; avec mon smartphone (Android uniquement)" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je ramène mon Vélib’ dans une station</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/_jjqjgpcA70" title="Je ramène mon Vélib’ dans une station" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>J'utilise un Vélib' électrique</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/V5F-ryp9k-Y" title="J&#39;utilise un Vélib&#39; électrique" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je fais une pause pendant mon trajet</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/gLBd4Pe9-RY" title="Je fais une pause pendant mon trajet" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br></br>
+        <h3>Je restitue mon Vélib’ dans une Station+</h3>
+        <iframe width="448" height="315" src="https://www.youtube.com/embed/lnmEMH8aJvc" title="Je restitue mon Vélib&#39; dans une Station+" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <h4>Plus d'information sur <a href = "https://www.velib-metropole.fr/">Vélib'</a> </h4>
+        </div>
     )
 }
 export default Home
